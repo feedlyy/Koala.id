3825ba137e6d154b88f700288600c1f3566b2583
Fadli <mnurfadd@gmail.com>
2020-11-18 19:08:30 +0700

    Added route and port for serve a server

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main () {
+import (
+	"Koala/Rest/app/controllers"
+	"net/http"
+)
 
+func main () {
+	//list of routes
+	http.HandleFunc("/register", controllers.Register)
+
+	//serve a server
+	_ = http.ListenAndServe(":8000", nil)
 }
